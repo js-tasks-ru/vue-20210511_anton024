42f1273a112df331edef7ffe83b4ef2ab3cc7e9e
antonBagryanov <abagryanov@gmail.com>
2021-05-19 23:18:29 +0300

    Add solution for 01-basics/04-MeetupPageDraft

--- 01-basics/04-MeetupPageDraft/script.js
+++ 01-basics/04-MeetupPageDraft/script.js
@@ -3,6 +3,9 @@ import Vue from './vendor/vue.esm.browser.js';
 /** URL адрес API */
 const API_URL = 'https://course-vue.javascript.ru/api';
 
+const fetchMeetup = (id) => fetch(`${API_URL}/meetups/${id}`).then((res) => res.json());
+const checkUrl = (url) => fetch(url).then((res) => res.status);
+
 /** ID митапа для примера; используйте его при получении митапа */
 const MEETUP_ID = 6;
 
@@ -44,4 +47,60 @@ const agendaItemIcons = {
   other: 'cal-sm',
 };
 
-// Требуется создать Vue приложение
+new Vue({
+  data() {
+    return {
+      rawMeetup: null,
+      meetupImageUrl: null,
+    };
+  },
+
+  computed: {
+    meetup() {
+      if (!this.rawMeetup) {
+        return null;
+      }
+      const tmpMeetup = { ...this.rawMeetup };
+      tmpMeetup.agenda.map((item) => {
+        item.title = this.checkAgendaItemTitle(item.title, item.type);
+        item.icon = this.getAgendaItemIcon(item.type);
+      });
+      return { ...tmpMeetup, cover: this.meetupImageUrl, coverStyle: { '--bg-url': this.meetupImageUrl } };
+    },
+
+    localizedDate() {
+      if (!this.rawMeetup) {
+        return null;
+      }
+      return new Date(this.rawMeetup.date).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+  },
+
+  mounted() {
+    fetchMeetup(MEETUP_ID)
+      .then((meetup) => {
+        this.rawMeetup = meetup;
+        return meetup.id;
+      })
+      .then((meetupId) => {
+        const url = getImageUrlByImageId(meetupId);
+        checkUrl(url).then((status) => {
+          this.meetupImageUrl = status === 200 ? `url(${url})` : ``;
+        });
+      });
+  },
+
+  methods: {
+    checkAgendaItemTitle(itemTitle, itemType) {
+      return itemTitle ? itemTitle : agendaItemDefaultTitles[itemType];
+    },
+
+    getAgendaItemIcon(itemType) {
+      return `/assets/icons/icon-${agendaItemIcons[itemType]}.svg`;
+    },
+  },
+}).$mount('#app');
