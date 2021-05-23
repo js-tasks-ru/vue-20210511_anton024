ebdd5ccaeca1d121aa9cf1ccc7da5bb03546540c
antonBagryanov <abagryanov@gmail.com>
2021-05-23 21:18:10 +0300

    Add solution for 02-components/06-MeetupView

--- 02-components/06-MeetupView/MeetupView.js
+++ 02-components/06-MeetupView/MeetupView.js
@@ -1,26 +1,46 @@
-// import MeetupCover from './MeetupCover.js';
-// import MeetupDescription from './MeetupDescription.js';
-// import MeetupAgenda from './MeetupAgenda.js';
-// import MeetupInfo from './MeetupInfo.js';
-// import { getImageUrlByImageId } from './data.js';
+import MeetupCover from './MeetupCover.js';
+import MeetupDescription from './MeetupDescription.js';
+import MeetupAgenda from './MeetupAgenda.js';
+import MeetupInfo from './MeetupInfo.js';
+import { getImageUrlByImageId } from './data.js';
 
 const MeetupView = {
   name: 'MeetupView',
 
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  components: {
+    MeetupCover,
+    MeetupDescription,
+    MeetupAgenda,
+    MeetupInfo,
+  },
+
+  computed: {
+    meetupImage() {
+      return this.meetup.imageId && getImageUrlByImageId(this.meetup.imageId);
+    },
+  },
+
   template: `
     <div>
-      <!-- meetup cover -->
+      <meetup-cover :link="meetupImage" :title="meetup.title"></meetup-cover>
       <div class="container">
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
-            <!-- meetup description -->
+            <meetup-description :description="meetup.description"></meetup-description>
 
             <h3>Программа</h3>
-            <!-- meetup agenda -->
+            <meetup-agenda :agenda="meetup.agenda"></meetup-agenda>
           </div>
           <div class="meetup__aside">
-            <!-- meetup info -->
+            <meetup-info :organizer="meetup.organizer" :place="meetup.place" :date="new Date(meetup.date)"></meetup-info>
           </div>
         </div>
       </div>
