8059700f76de7329a6da139d7a0853d643f15565
antonBagryanov <abagryanov@gmail.com>
2021-05-24 00:14:37 +0300

    Add solution for 02-components/07-MeetupPageComponent

--- 02-components/07-MeetupPageComponent/MeetupPage.js
+++ 02-components/07-MeetupPageComponent/MeetupPage.js
@@ -1,10 +1,26 @@
-// import MeetupView from './MeetupView.js';
-// import { MEETUP_ID, fetchMeetup } from './data.js';
+import MeetupView from './MeetupView.js';
+import { MEETUP_ID, fetchMeetup } from './data.js';
 
 const MeetupPage = {
   name: 'MeetupPage',
 
-  template: `<div></div>`,
+  data() {
+    return {
+      meetup: null,
+    };
+  },
+
+  mounted() {
+    fetchMeetup(MEETUP_ID).then((meetup) => (this.meetup = meetup));
+  },
+
+  components: {
+    MeetupView,
+  },
+
+  template: `<div v-if="meetup">
+    <meetup-view :meetup="meetup"></meetup-view>
+  </div>`,
 };
 
 export default MeetupPage;
